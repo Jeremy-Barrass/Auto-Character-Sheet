75a9bc2c75905a1af75b3c01f27b8b127a512555
Flight-of-Dragons <jeremy.barrass@gmail.com>
2016-04-26 12:44:04 +0100

    Removed all directories unrelated to the project.

--- Bust_A_DotCom/Testing/startPlayingTestDrive.java
+++
@@ -1,9 +0,0 @@
-public class startPlayingTestDrive {
-
-    public static void main (String[] args) {
-
-        DotComBust dcbt = new DotComBust();
-
-        ui = "C5";
-
-        if (helper.getUserInput(ui) 
