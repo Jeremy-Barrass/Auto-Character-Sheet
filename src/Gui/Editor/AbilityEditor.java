a2b81013c8db227c79c3e57c5abbec052bed837d
Jeremy Barrass <jeremy.barrass@gmail.com>
2018-01-10 22:21:58 +0000

    UpdateAbilities() in E

--- src/Gui/Editor/AbilityEditor.java
+++ src/Gui/Editor/AbilityEditor.java
@@ -1,16 +1,21 @@
 package Gui.Editor;
 
 import Gui.JColumn;
+import Listeners.AbilityEditorListener;
+import RulesLogic.Abilities;
 import SheetConstants.AbilityNames;
 import interfaces.iAbilities;
 import interfaces.iDisplay;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.Serializable;
 
 public class AbilityEditor extends JPanel implements iDisplay, Serializable {
     private iAbilities _abilities;
+    private ActionListener _listener;
     private JButton saveButton;
     private JColumn titles;
     private JColumn fields;
@@ -20,7 +25,10 @@ public class AbilityEditor extends JPanel implements iDisplay, Serializable {
     public AbilityEditor(iAbilities abilities) {
         setLayout(new BorderLayout());
         _abilities = abilities;
+        _listener = new AbilityEditorListener();
     }
+
+    public AbilityEditor() {this(new Abilities())}
 
     public void Display() {
         titles = new JColumn();
@@ -29,11 +37,21 @@ public class AbilityEditor extends JPanel implements iDisplay, Serializable {
         for(String ability : AbilityNames.ListAbilityNames()) {
             title = new JLabel(ability);
             titles.add(title);
-            field = new JTextField();
+            field = new JTextField(4);
             fields.add(field);
         }
-        add(BorderLayout.WEST, titles);
+        add(BorderLayout.CENTER, titles);
         add(BorderLayout.CENTER, fields);
         add(BorderLayout.SOUTH, saveButton);
+    }
+
+    private void UpdateAbilities() {
+        _abilities.SetAbilityScore();
+    }
+
+    class AbilityEditorListener implements ActionListener {
+        public void actionPerformed(ActionEvent event) {
+            UpdateAbilities();
+        }
     }
 }
--- test/TestAbilityEditor.java
+++ test/TestAbilityEditor.java
@@ -4,16 +4,23 @@ import org.junit.Assert;
 import org.junit.Test;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 
+import java.awt.event.ActionListener;
+
 import static org.mockito.Mockito.mock;
 
 @PrepareForTest(AbilityEditor.class)
 
 public class TestAbilityEditor {
-    Abilities mockAbilities = mock(Abilities.class);
-    AbilityEditor editor = new AbilityEditor(mockAbilities);
 
     @Test
     public void testUpdateAbilities_WhenItReceivesANullValue_ItDoesNotChangeTheScores() {
+        // Arrange
+        Abilities mockAbilities = mock(Abilities.class);
+        ActionListener mockListener = mock(ActionListener.class);
 
+        // Act
+
+
+        // Assert
     }
 }
