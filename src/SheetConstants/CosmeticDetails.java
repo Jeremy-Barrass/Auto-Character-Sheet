4def54864d82a5dd914580fdebbf1974c26c7e82
Jeremy Barrass <jeremy.barrass@gmail.com>
2018-02-04 15:37:48 +0000

    Cosmetic Details Terminology added.
    This will be used by the display and editing panels in the GUI.

---
+++ src/SheetConstants/CosmeticDetails.java
@@ -0,0 +1,15 @@
+package SheetConstants;
+
+public final class CosmeticDetails {
+    public static final String CharacterName = "Character Name";
+    public static final String Player = "Player";
+    public static final String Deity = "Deity";
+    public static final String Homeland = "Homeland";
+    public static final String Gender = "Gender";
+    public static final String Height = "Height";
+    public static final String Weight = "Weight";
+    public static final String Hair = "Hair";
+    public static final String Eyes = "Eyes";
+    public static final String Languages = "Languages";
+
+}
