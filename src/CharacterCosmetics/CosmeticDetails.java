7db1e89871e255a7076548ae83d631ad5a87a14f
Jeremy Barrass <jeremy.barrass@gmail.com>
2018-02-06 22:54:38 +0000

    setConfigDetails now working correctly
    Named details are set, provided the intended value always follows
    the intended key.  Unnamed are simply set in the same order as the
    labels appear in the character sheet.

--- src/CharacterCosmetics/CosmeticDetails.java
+++ src/CharacterCosmetics/CosmeticDetails.java
@@ -11,12 +11,15 @@ import java.util.Observable;
 public class CosmeticDetails extends Observable implements iCosmeticDetails, Serializable {
     private static Hashtable<String, String> Details;
     private static ArrayList<String> checkList;
+    private ArrayList<String> labelKeys;
 
     public CosmeticDetails() {
         Details = new Hashtable<String, String>();
         checkList = new ArrayList<>();
+        labelKeys = new ArrayList<>();
         for (String label : CosmeticDetailsLabels.listCosmeticDetails()) {
             Details.put(label, "");
+            labelKeys.add(label);
             checkList.add(label.toLowerCase());
         }
     }
@@ -33,24 +36,40 @@ public class CosmeticDetails extends Observable implements iCosmeticDetails, Ser
     }
 
     public void setConfigDetails(String[] details) {
-        for (int x = 0; x < details.length-1; x++) {
-            if (checkList.contains(details[x].toLowerCase())) {
-                Details.put(details[x], details[x+1]);
-                details[x] = null;
-                details[x+1] = null;
-            }
-        }
-        for (int y = 0; y < details.length; y++) {
-            if (details[y] != null) {
-                String key = (String) Details.keySet().toArray()[y];
-                if (Details.get(key).isEmpty()) {
-                    Details.put(key, details[y]);
-                }
-            }
-        }
-        for (String key : Details.keySet()) {
-            setDetail(key, Details.get(key));
-        }
+        ArrayList<String> dConfig = new ArrayList<>();
+        setDetailConfig(details, dConfig);
+        setNamedDetails(dConfig, details);
+        dConfig.clear();
+        setDetailConfig(details, dConfig);
+        setUnamedDetails(dConfig);
+    }
 
+    private void setDetailConfig(String[] details, ArrayList<String> dConfig) {
+        for (String detail : details) {
+            if (!detail.isEmpty()) {
+                dConfig.add(detail);
+            }
+        }
+    }
+
+    private void setNamedDetails(ArrayList<String> dConfig, String[] details) {
+        for (String item : dConfig) {
+            if (checkList.contains(item.toLowerCase())) {
+                int x = dConfig.indexOf(item);
+                Details.put(item, dConfig.get(x+1));
+                details[x] = "";
+                details[x+1] = "";
+            }
+        }
+    }
+
+    private void setUnamedDetails(ArrayList<String> dConfig) {
+        for (String key : labelKeys) {
+            if (Details.get(key).isEmpty()) {
+                int y = labelKeys.indexOf(key);
+                if (y >= dConfig.size()) return;
+                Details.put(key, dConfig.get(y));
+            }
+        }
     }
 }
